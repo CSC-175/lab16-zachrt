14c592a3725ca02fb981e761c9fbb788288fb88a
Zachay Thomas <zachrt@umich.edu>
2025-03-20 12:44:07 -0400

    First Attempt

--- lab16.cpp
+++ lab16.cpp
@@ -1,1 +1,22 @@
 // Code to implement the gcf function goes here
+int gcf(int a, int b)
+{
+    int smaller, highest = 1;
+    if (a > b)
+    {
+        smaller = a;
+    } else
+    {
+        smaller = b;
+    }
+
+    for (int i = highest; i <= smaller; i++) {
+        if (a% i == 0 && b % i == 0)
+        {
+            highest = i;
+        }
+    }
+
+    return highest;
+
+}
